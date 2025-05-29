c2796b06f711dcffe715ac6e3aaa5114157c8230
riley <rileylnorton05@gmail.com>
2025-05-29 22:53:39 +0100

    fix: remove unused imports

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,8 +1,5 @@
-import { Image } from "expo-image";
 import { Platform, StyleSheet, View } from "react-native";
 
-import { HelloWave } from "@/components/HelloWave";
-import ParallaxScrollView from "@/components/ParallaxScrollView";
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
 
