d937eb55773ed0e1b6861fb28d8262a9ebae344f
Dylan Beharall <114403283+Herblawunf@users.noreply.github.com>
2025-06-04 22:46:37 +0100

    fix : Fix symbols on navigation bar

--- app/(tenant_tabs)/_layout.jsx
+++ app/(tenant_tabs)/_layout.jsx
@@ -37,60 +37,48 @@ export default function TabLayout() {
           ),
         }}
       />
-      {
-        <Tabs.Screen
-          name="notifications"
-          options={{
-            title: "Notifications",
-            tabBarIcon: ({ color }) => (
-              <IconSymbol size={28} name="paperplane.fill" color={color} />
-            ),
-          }}
-        />
-      }
-      ,
-      {
-        <Tabs.Screen
-          name="contact"
-          options={{
-            title: "Contact",
-            tabBarIcon: ({ color }) => (
-              <IconSymbol size={28} name="envelope.fill" color={color} />
-            ),
-          }}
-        />
-      }
-      ,
-      {
-        <Tabs.Screen
-          name="chat"
-          options={{
-            title: "Chat",
-            tabBarIcon: ({ color }) => (
-              <IconSymbol size={28} name="chat.bubble.fill" color={color} />
-            ),
-          }}
-        />
-      }
-      ,
-      {
-        <Tabs.Screen
-          name="expenses"
-          options={{
-            title: "Expenses",
-            tabBarIcon: ({ color }) => (
-              <IconSymbol size={28} name="chat.bubble.fill" color={color} />
-            ),
-          }}
-        />
-      }
-      ,
+      <Tabs.Screen
+        name="notifications"
+        options={{
+          title: "Notifications",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="bell.fill" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="contact"
+        options={{
+          title: "Contact",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="envelope.fill" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="chat"
+        options={{
+          title: "Chat",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="message.fill" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="expenses"
+        options={{
+          title: "Expenses",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="creditcard.fill" color={color} />
+          ),
+        }}
+      />
       <Tabs.Screen
         name="settings"
         options={{
           title: "Settings",
           tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="envelope.fill" color={color} />
+            <IconSymbol size={28} name="gearshape.fill" color={color} />
           ),
         }}
       />
