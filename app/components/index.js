ae86fcff4b2f52ea3eaf6cda8d30467f6fb6b2ad
Damian Spigut <damino@Damians-MacBook-Pro.local>
2025-06-18 01:27:38 +0100

    feat: tenant satisfaction button on main home page

--- app/components/index.js
+++ app/components/index.js
@@ -1,6 +1,8 @@
 export { default as NotionCalendar } from './NotionCalendar';
+export { default as TenantSatisfaction } from './TenantSatisfaction';
 
 // Default export for the components module
 export default {
-  NotionCalendar: require('./NotionCalendar').default
+  NotionCalendar: require('./NotionCalendar').default,
+  TenantSatisfaction: require('./TenantSatisfaction').default
 }; 
